ac1fe375943f917cf1ca97680405c778678416f5
SanderHoudijk <fristislekker@proton.me>
2025-01-14 15:12:55 +0100

    Add app store buttons and enhance homepage layout with new styles

--- src/index.php
+++ src/index.php
@@ -9,18 +9,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/style.css">
     <title>Ctrl Alt Elite</title>
+    <script src="https://kit.fontawesome.com/24be3f9595.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <nav>
-        <ul>
-            <li><span>Ctrl Alt Elite</span></li>
+        <ul class="left-nav">
+            <li><img src="assets/ctrlaltelite.png"></li> <!-- <span>Ctrl Alt Elite</span> -->
             <li><a href="index.php">Home</a></li>
             <li><a href="discover.php">Discover</a></li>
             <li><a href="dashboard.php">Dashboard</a></li>
-            <li class="right"><a href="login.php">Login</a></li>
-            <li class="right"><a href="signup.php">Signup</a></li>
-            <li class="right">
+        </ul>
+        <ul class="right-nav">
+            <li><a href="login.php">Login</a></li>
+            <li><a href="signup.php">Signup</a></li>
+            <li><i class="fa-solid fa-globe"></i>
                 <select name="language" id="language">
                     <option value="english">EN</option>
                     <option value="dutch">NL</option>
@@ -28,6 +31,37 @@
             </li>
         </ul>
     </nav>
+    <div class="main">
+        <span style="color: var(--secondary)"><b>LETS START THE QUIZ!</b></span>
+        <h2>Step into a Realm of <br>Infinite Quiz Excitement</h2>
+        <h3>Ready to test your knowledge and have fun? Challenge <br> friends, learn new things, and Kahoot your way to
+            victory!</h3>
+
+        <div class="opaImage">
+            <img src="assets/opa.png" alt="opa">
+        </div>
+
+        <button type="submit" class="hostnow" onclick="sendToDashboard()">Host Now!</button> <a class="underline"
+            href="discover.php">Create Your Quiz</a>
+            
+        <h4>Get our app now on:</h4>
+        <div class="appStore">
+            <button>
+                <img src="assets/play_store_grey.png" alt="Google Play">
+                <span>Play Store</span>
+            </button>
+            <button>
+                <img src="assets/app_store_grey.png" alt="App Store">
+                <span>App Store</span>
+            </button>
+        </div>
+    </div>
+    
+    <script>
+        function sendToDashboard() {
+            window.location.href = "dashboard.php";
+        }
+    </script>
 </body>
 
 </html>
