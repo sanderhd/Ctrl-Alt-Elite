4b2d54f978019bd771a4da37598d328d79ebd0ed
SanderHoudijk <fristislekker@proton.me>
2025-01-15 09:46:11 +0100

    Increase question time limits and improve timer handling in quiz functionality

--- src/level1/JS/script.js
+++ src/level1/JS/script.js
@@ -11,21 +11,21 @@ const questions = [
         question: "Wat is de grootste planeet in ons zonnestelsel?",
         options: ["Aarde", "Mars", "Jupiter", "Saturnus"],
         goodAnswer: "Jupiter",
-        time: 10,
+        time: 30,
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Jupiter_and_its_shrunken_Great_Red_Spot.jpg/800px-Jupiter_and_its_shrunken_Great_Red_Spot.jpg"
     },
     {
         question: "Wat is de hoofdstad van Frankrijk?",
         options: ["Lyon", "Marseille", "Parijs", "Nice"],
         goodAnswer: "Parijs",
-        time: 10,
+        time: 15,
         image: "https://www.zininfrankrijk.nl/wp-content/uploads/2019/03/Parijs-%C3%8Ele-de-France-shutterstock_710380270-660x330.jpg"
     },
     {
         question: "Wie schreef 'Romeo en Julia'?",
         options: ["Charles Dickens", "William Shakespeare", "Mark Twain", "Jane Austen"],
         goodAnswer: "William Shakespeare",
-        time: 10,
+        time: 5,
         image: "https://hips.hearstapps.com/hmg-prod/images/william-shakespeare-194895-1-402.jpg?crop=1xw:1.0xh;center,top&resize=980:*"
     },
     {
@@ -39,35 +39,35 @@ const questions = [
         question: "Wat is de hoofdstad van Japan?",
         options: ["Osaka", "Kyoto", "Tokyo", "Hiroshima"],
         goodAnswer: "Tokyo",
-        time: 10,
+        time: 20,
         image: "https://res.cloudinary.com/hello-tickets/image/upload/c_limit,f_auto,q_auto,w_1300/v1666062109/nfvpcqvorybqcin4j5en.jpg"
     },
     {
         question: "Wie ontdekte penicilline?",
         options: ["Marie Curie", "Alexander Fleming", "Louis Pasteur", "Isaac Newton"],
         goodAnswer: "Alexander Fleming",
-        time: 10,
+        time: 15,
         image: "https://www.nobelprize.org/images/fleming-13037-content-portrait-mobile-tiny.jpg"
     },
     {
         question: "Wat is de langste rivier ter wereld?",
         options: ["Nijl", "Amazonerivier", "Yangtze", "Mississippi"],
         goodAnswer: "Nijl",
-        time: 10,
+        time: 30,
         image: "https://www.natgeojunior.nl/wp-content/uploads/2017/07/Nijl4.jpg"
     },
     {
         question: "Wat is de hoofdstad van Italië?",
         options: ["Milaan", "Rome", "Venetië", "Napels"],
         goodAnswer: "Rome",
-        time: 10,
+        time: 40,
         image: "https://worldstrides.com/wp-content/uploads/2015/07/api268.jpg"
     },
     {
         question: "Wie schilderde de Mona Lisa?",
         options: ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Claude Monet"],
         goodAnswer: "Leonardo da Vinci",
-        time: 10,
+        time: 30,
         image: "https://hips.hearstapps.com/hmg-prod/images/portrait-of-leonardo-da-vinci-1452-1519-getty.jpg?crop=1xw:1.0xh;center,top&resize=980:*"
     }
 ];
@@ -80,35 +80,34 @@ const questionDisplay = document.getElementById("questionDisplay");
 const questionImage = document.getElementById('questionImage');
 const timeDisplay = document.getElementById('timeDisplay');
 
-let timer; // Declareer de timer variabele buiten de functie
+let timer;
 
 function countdown() {
     let time = questions[questionNumber].time;
 
-    // Stop de vorige timer als die bestaat
+    // timer clearen als hij bestaat
     if (timer) {
         clearInterval(timer);
     }
 
-    // Stel een interval timer in die elke 1000 milliseconden wordt uitgevoerd
+    // Timer die elke seconde omlaag gaat
     timer = setInterval(() => {
         timeDisplay.innerHTML = time + ' sec';
         time--;
 
-        // Stop de timer als de tijd op is
+        // stop de tijd als hij op 0 is
         if (time <= 0) {
             clearInterval(timer);
-            checkAnswer();
+            checkAnswer(); // controleer het antwoord als de tijd 0 is
         }
     }, 1000);
 }
-
 
 function loadQuestion() {
     const question = questions[questionNumber];
     questionDisplay.innerText = question.question;
     questionImage.src = question.image;
-    countdown(); // Start de countdown voor de nieuwe vraag
+    countdown(); // start het aftellen bij het laden van een nieuwe vraag.
 
     const optionButtons = document.querySelectorAll('.optionButton');
     optionButtons.forEach((button, index) => {
@@ -123,11 +122,12 @@ function selectAnswer(button) {
     const optionButtons = document.querySelectorAll('.optionButton');
     optionButtons.forEach(btn => btn.classList.remove('selected'));
     button.classList.add('selected');
+    clearInterval(timer); // de timer stoppen als er een antwoord gekozen is zodat hij niet verder gaat bij de volgende vraag.
     checkAnswer(button.value);
 }
 
 // controleer of het gegeven antwoord goed is
-function checkAnswer(selectedAnswer) {
+function checkAnswer(selectedAnswer = null) {
     const question = questions[questionNumber];
     const isCorrect = selectedAnswer === question.goodAnswer;
 
@@ -141,7 +141,7 @@ function checkAnswer(selectedAnswer) {
     if (questionNumber < questions.length) {
         loadQuestion();
     } else {
-        // Redirect to result.php with the results
+        // sturen naar result.php met het goede en foute antwoorden
         window.location.href = `result.php?goodAnswers=${goodAnswers}&wrongAnswers=${wrongAnswers}`;
     }
 }
